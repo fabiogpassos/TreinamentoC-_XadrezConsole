86d09195453eac28597e0c984d17821482aa2887
Fabio Gomes Passos <fgpassos@gmail.com>
2021-04-28 11:44:32 -0300

    Começando a implementar o tabuleiro.

--- XadrezConsole/Tabuleiro/Posicao.cs
+++ XadrezConsole/Tabuleiro/Posicao.cs
@@ -1,23 +1,21 @@
-﻿
-
-namespace Tabuleiro
+﻿namespace tabuleiro
 {
     class Posicao
     {
-        public int Linha { get; set; }
-        public int Coluna { get; set; }
+        public int linha { get; set; }
+        public int coluna { get; set; }
 
         public Posicao(int linha, int coluna)
         {
-            Linha = linha;
-            Coluna = coluna;
+            this.linha = linha;
+            this.coluna = coluna;
         }
 
         public override string ToString()
         {
-            return Linha
+            return linha
                 + ", "
-                + Coluna;
+                + coluna;
         }
     }
 }
